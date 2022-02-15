a627f117c1bff2a84de22b9df6ae12645969099a
William <wpineda@cultura.gob.sv>
2022-02-15 00:45:25 -0600

    Creacion Models+Controllers+Seeders+Migrations

---
+++ app/Http/Controllers/OrganizationalUnitController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrganizationalUnit;
+use Illuminate\Http\Request;
+
+class OrganizationalUnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrganizationalUnit  $organizationalUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrganizationalUnit $organizationalUnit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OrganizationalUnit  $organizationalUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrganizationalUnit $organizationalUnit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrganizationalUnit  $organizationalUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrganizationalUnit $organizationalUnit)
+    {
+        //
+    }
+}
---
+++ database/seeders/MonthlyClosingSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MonthlyClosingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
